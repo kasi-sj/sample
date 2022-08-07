a7d216a37ac1187886800af97763ce1948cab6fa
kasi_sj <jkasinathan007@gmail.com>
2022-08-07 20:02:29 +0530

    colume and row widged

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 72, 72, 71),
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
-          title: const Center(
-            child: Text(
-              'THIS IS V',
-            ),
+        backgroundColor: Color.fromARGB(255, 29, 136, 152),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                padding: EdgeInsets.all(40),
+                margin: const EdgeInsets.only(right: 10),
+                width: 100,
+                height: 770,
+                color: Colors.red,
+                child: const Text('hi'),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(40),
+                    margin: const EdgeInsets.only(right: 10),
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                    child: const Text('hi'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(40),
+                    margin: const EdgeInsets.only(right: 10),
+                    width: 100,
+                    height: 100,
+                    color: Color.fromARGB(255, 211, 235, 149),
+                    child: const Text('hi'),
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.all(40),
+                margin: const EdgeInsets.only(right: 10),
+                width: 100,
+                height: 770,
+                color: Colors.blue,
+                child: const Text('hi'),
+              ),
+            ],
           ),
         ),
-        body: Center(
-          child: Image.asset('asset/fun.jpg'),
-        ),
       ),
-    ),
-  );
+    );
+  }
 }
